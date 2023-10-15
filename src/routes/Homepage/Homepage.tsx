9afacb42b4bb3e4635611f6d4c997f714fd6e9aa
Wasin Wachirapusitanun <wasin.ww1402@gmail.com>
2023-10-15 12:11:42 +0700

    feat: add point dot intopage

--- src/routes/Homepage/Homepage.tsx
+++ src/routes/Homepage/Homepage.tsx
@@ -7,6 +7,20 @@ const Homepage = () => {
         <div className={'ux-thailand-conf'}>UX Thailand</div>
         <div className={'ux-thailand-conf'}>Conference 2024</div>
         <div className={'coming-soon'}>Coming Soon !</div>
+
+        {/* dot point */}
+        <img
+          className={'point-dot point-dot-green'}
+          src={'/assets/images/point-dot/green.svg'}
+        />
+        <img
+          className={'point-dot point-dot-blue'}
+          src={'/assets/images/point-dot/blue.svg'}
+        />
+        <img
+          className={'point-dot point-dot-magenta'}
+          src={'/assets/images/point-dot/magenta.svg'}
+        />
       </div>
     </div>
   );
