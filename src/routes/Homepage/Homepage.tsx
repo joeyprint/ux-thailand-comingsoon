16f1ef6b89a5f5b28bf1797fdeeddab2b7ac5bfb
Wasin Wachirapusitanun <wasin.ww1402@gmail.com>
2023-10-15 11:41:00 +0700

    feat: create a home page ux thailand

---
+++ src/routes/Homepage/Homepage.tsx
@@ -0,0 +1,15 @@
+import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <div id={'homepage'}>
+      <div className="title-section">
+        <div className={'ux-thailand-conf'}>UX Thailand</div>
+        <div className={'ux-thailand-conf'}>Conference 2024</div>
+        <div className={'coming-soon'}>Coming Soon !</div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
