03fe99bd4722a8fdebd7c7e0bdf94c9f838c7b91
Wasin Wachirapusitanun <wasin.ww1402@gmail.com>
2023-10-15 11:54:41 +0700

    feat: create navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import './Narbar.css';
+
+const Navbar = () => {
+  return (
+    <div id={'navbar-section'}>
+      <div className={'container'}>
+        <img
+          className={'uxth-logo'}
+          src={'/assets/images/UXTH Conference Logo.svg'}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
