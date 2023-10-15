67311146f6d88e5bdae36a48a7959e357ec4c771
Wasin Wachirapusitanun <wasin.ww1402@gmail.com>
2023-10-15 12:41:59 +0700

    feat: integrate background image

--- src/routes/Homepage/Homepage.tsx
+++ src/routes/Homepage/Homepage.tsx
@@ -3,24 +3,36 @@ import './Homepage.css';
 const Homepage = () => {
   return (
     <div id={'homepage'}>
+      {/* background image */}
+      <img
+        className={'wind-wheel-icon'}
+        src={'/assets/images/thai-signature-icon/wind-wheel.svg'}
+      />
+      <img
+        className={'elephant-icon'}
+        src={'/assets/images/thai-signature-icon/elephant.svg'}
+      />
+
       <div className={'title-section'}>
+        {/* dot point */}
+        <div className={'dot-box'}>
+          <img
+            className={'point-dot point-dot-green'}
+            src={'/assets/images/point-dot/green.svg'}
+          />
+          <img
+            className={'point-dot point-dot-magenta'}
+            src={'/assets/images/point-dot/magenta.svg'}
+          />
+          <img
+            className={'point-dot point-dot-blue'}
+            src={'/assets/images/point-dot/blue.svg'}
+          />
+        </div>
+
         <div className={'ux-thailand-conf'}>UX Thailand</div>
         <div className={'ux-thailand-conf'}>Conference 2024</div>
         <div className={'coming-soon'}>Coming Soon !</div>
-
-        {/* dot point */}
-        <img
-          className={'point-dot point-dot-green'}
-          src={'/assets/images/point-dot/green.svg'}
-        />
-        <img
-          className={'point-dot point-dot-blue'}
-          src={'/assets/images/point-dot/blue.svg'}
-        />
-        <img
-          className={'point-dot point-dot-magenta'}
-          src={'/assets/images/point-dot/magenta.svg'}
-        />
       </div>
     </div>
   );
